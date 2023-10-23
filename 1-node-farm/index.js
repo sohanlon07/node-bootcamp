08fb392a6ca3d1e1ddfab4c6ee6cc785d58c47ba
Shane OHanlon <sohanlon07@gmail.com>
2023-10-23 20:27:33 +0100

    sync vs async, callbacks

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,26 @@
 const fs = require('fs')
+const http = require('http')
+//Blocking, synchronous
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn)
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn)
+// const textOut = `This is a what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File written!')
 
-const textOut = `This is a what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log('File written!')
+//Non-Blocking, synchronous
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if(err) return console.log('ERROR! 💥')
+    
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2)
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3)
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written 😀')
+            })
+        })
+    })
+})
+console.log('Will read file!')
