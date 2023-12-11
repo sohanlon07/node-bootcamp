5f5e37588794a906ea7f74aa74878bcdd6f9a3a6
Shane OHanlon <sohanlon07@gmail.com>
2023-12-11 19:32:31 +0000

    feat: add slugify

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate')
+
+
 /////////////////////////////////////////////////////////////
 // Files
 
@@ -49,6 +52,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
+
+const slugs = dataObject.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
